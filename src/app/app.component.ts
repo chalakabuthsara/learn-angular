61372d3ff319a5f2a82bf28daff1422699859b81
chalakabuthsara <cbuthsara@gmail.com>
2024-10-17 20:00:37 +0530

    fourth commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [HomeComponent, RouterModule, RouterLink, RouterOutlet],
   template: `
-    <main> jj
+    <main>
       <a [routerLink] ="['/']">
     <header class="brand-name">
         <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
